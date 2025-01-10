031cd2dacc0777a05610c9d6e039d462aa296530
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2025-01-10 13:12:28 -0800

    added content for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,93 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import content from "../content/HomePageContent.json"
 
 const HomePage = () => {
   const chunks = [
-    // First chunk: Image on the left and text on the right
-    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", scrollSnapAlign: "start" }}>
-      <Typography variant="h2" sx={{ marginRight: 2 }}>Welcome!</Typography>
-      <Typography variant="h4">Insert Name</Typography>
+    // Welcome Section: Image on the left and text on the right
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", scrollSnapAlign: "start", gap: 4 }}>
+      <Typography variant="h4">
+        {content.welcomeSection.name}
+        </Typography>
+      <Typography variant="h2" sx={{  marginTop: 2, marginLeft: "25%", transform: "translateX(-25%)" }}>
+        {content.welcomeSection.greeting}
+        </Typography>
     </Box>,
 
-    // Second chunk: Text on the left, image on the right
+    // Intro Section: Text on the left, image on the right
     <Box sx={{ display: "flex", justifyContent: "space-between", height: "100vh", scrollSnapAlign: "start" }}>
       <Box>
-        <Typography>Lorem ipsum dolor sit amet.</Typography>
-        <Typography>Consectetur adipiscing elit.</Typography>
+        <Typography>
+          {content.introSection}
+        </Typography>
+        
       </Box>
       <Box component="img" src="" alt="Placeholder" sx={{ width: "50%" }} />
     </Box>,
 
-    // Third chunk: Two paragraphs centered
-    <Box sx={{ textAlign: "center", height: "100vh", scrollSnapAlign: "start" }}>
-      <Typography>Lorem ipsum dolor sit amet.</Typography>
-      <Typography>Consectetur adipiscing elit.</Typography>
+    // Professional Journey: PayPal Part 1
+    <Box sx={{ textAlign: "left", height: "100vh", scrollSnapAlign: "start" }}>
+      <Typography variant="h2">
+        {content.professionalJourneySection.title}
+      </Typography>
+      <Typography sx={{ fontWeight: "bold" }}>
+        {content.professionalJourneySection.paypalSubSection.title}
+      </Typography>
+      <Typography sx={{ fontStyle: "italic" }}>
+        {content.professionalJourneySection.paypalSubSection.date}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.paypalSubSection.paragraph1}
+      </Typography>
     </Box>,
 
-    // Fourth chunk: Two paragraphs centered
-    <Box sx={{ textAlign: "center", height: "100vh", scrollSnapAlign: "start" }}>
-      <Typography>Lorem ipsum dolor sit amet.</Typography>
-      <Typography>Consectetur adipiscing elit.</Typography>
+    // Professional Journey: PayPal Part 2
+    <Box sx={{ textAlign: "left", height: "100vh", scrollSnapAlign: "start" }}>
+      <Typography>
+        {content.professionalJourneySection.paypalSubSection.paragraph2}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.paypalSubSection.paragraph3}
+      </Typography>
     </Box>,
+
+    // Professional Journey: Knoldus
+    <Box sx={{ textAlign: "left", height: "100vh", scrollSnapAlign: "start" }}>
+      <Typography sx={{ fontWeight: "bold" }}>
+        {content.professionalJourneySection.knoldusSubSection.title}
+      </Typography>
+      <Typography sx={{ fontStyle: "italic" }}>
+        {content.professionalJourneySection.knoldusSubSection.date}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.knoldusSubSection.paragraph1}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.knoldusSubSection.paragraph2}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.knoldusSubSection.paragraph3}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.knoldusSubSection.paragraph4}
+      </Typography>
+    </Box>,
+
+    // Professional Journey: Mitacs
+    <Box sx={{ textAlign: "left", height: "100vh", scrollSnapAlign: "start" }}>
+       <Typography sx={{ fontWeight: "bold" }}>
+        {content.professionalJourneySection.mitacsSubsSection.title}
+      </Typography>
+      <Typography sx={{ fontStyle: "italic" }}>
+        {content.professionalJourneySection.mitacsSubsSection.date}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.mitacsSubsSection.paragraph1}
+      </Typography>
+      <Typography>
+        {content.professionalJourneySection.mitacsSubsSection.paragraph2}
+      </Typography>
+    </Box>
   ];
 
   return (
