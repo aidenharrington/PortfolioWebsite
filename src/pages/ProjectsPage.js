02325ba165f0acad7c591cd4c6e422833e355a33
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2025-01-20 12:48:52 -0800

    added content for projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { Box, Typography, Link } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import content from '../content/ProjectsPageContent.json'
 
 const ProjectsPage = () => {
   return (
     <Box sx={{ padding: 4 }}>
       <Typography variant="h4" gutterBottom>
-        Subscribr
+        {content.title}
       </Typography>
-      <Link href="#" target="_blank" sx={{ display: "flex", alignItems: "center" }}>
-        <GitHubIcon sx={{ marginRight: 1 }} />
-        GitHub Repository
+      <Link href="https://github.com/aidenharrington/subscribr-application" target="_blank" sx={{ display: "flex", alignItems: "center" }}>
+        {content.github}
+        <GitHubIcon sx={{ marginLeft: 1 }} />
       </Link>
+      <Typography sx={{ paddingTop: 4 }}>
+        {content.introParagraph}
+      </Typography>
+      <Typography sx={{ paddingTop: 4 }}>
+        {content.bodyParagraph1}
+      </Typography>
+      <Typography sx={{ paddingTop: 4 }}>
+        {content.bodyParagraph2}
+      </Typography>
+      <Typography sx={{ paddingTop: 4 }}>
+        {content.bodyParagraph3}
+      </Typography>
+      <Typography sx={{ paddingTop: 4 }}>
+        {content.conclusion}
+      </Typography>
     </Box>
-  );
-};
+)};
 
 export default ProjectsPage;
