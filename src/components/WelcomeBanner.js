e2f0298e3a7bded01c9358b228c982657f74591c
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2025-02-03 16:08:14 -0800

    added parallax background and re-integrated snap-scrolling effect

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-import profileImg from "../assets/images/profile_pic.png";
+import profileImg from "../assets/images/profile_pic_resized.jpg";
 import content from "../content/HomePageContent.json"
 import '../App.css';
 
@@ -67,33 +67,52 @@ const WelcomeBanner = () => {
     }, [])
 
     return (
-        <section className="banner" id="home">
-            <Container ref={containerRef}>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <div>
-                            <span className="tagline">{staticWelcomeText}</span>
-                            <h1>
-                                <span className="txt-rotate">
-                                    <span className="wrap">
-                                        {text}
-                                    </span>
-                                </span>
-                            </h1>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <div className="profile-img-container">
-                            <img
-                                src={profileImg}
-                                alt="Profile Picture"
-                                className="profile-img"
-                            />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+        <div ref={containerRef} className="container mt-5">
+            <div className="row align-items-center">
+                <div className="col-md-6 text-start">
+                    <h1 className="text-2xl font-semibold">{staticWelcomeText}</h1>
+                    <h1 className="text-xl font-medium text-gray-700 mt-2">{text}</h1>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center">
+                    <div className="profile-img-container">
+                        <img src={profileImg} alt="Profile" className="profile-img" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        // <section className="banner" id="home">
+        //     <Container ref={containerRef}>
+        //         <Row className="align-items-center">
+        //             <Col xs={12} md={6} xl={7}>
+        //                 <div>
+        //                     <span className="tagline">{staticWelcomeText}</span>
+        //                     <h1 style={{ minWidth: "350px", textAlign: "left", whiteSpace: "nowrap" }}>
+        //                         <span className="txt-rotate">
+        //                             <span className="wrap">
+        //                                 {text}
+        //                             </span>
+        //                         </span>
+        //                     </h1>
+        //                 </div>
+        //             </Col>
+        //             <Col xs={12} md={6} xl={5}>
+        //                 <div className="profile-img-container">
+        //                     <img
+        //                         src={profileImg}
+        //                         alt="Profile Picture"
+        //                         className="profile-img"
+        //                     />
+        //                 </div>
+        //             </Col>
+        //         </Row>
+        //     </Container>
+        // </section>
     );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,70 +1,56 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 import content from "../content/HomePageContent.json";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import WelcomeBanner from "../components/WelcomeBanner";
 
+import '../App.css';
+
 const HomePage = () => {
-  const chunks = [
-
-    // Welcome Section: Image on the left and text on the right
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100 scroll-snap-start gap-4">
-      <h4>{content.welcomeSection.name}</h4>
-      <h2 className="mt-2" style={{ marginLeft: "25%", transform: "translateX(-25%)" }}>
-        {content.welcomeSection.greeting}
-      </h2>
-    </div>,
-
-    // Intro Section: Text on the left, image on the right
-    <div className="d-flex justify-content-between vh-100 scroll-snap-start">
-      <div>
-        <p>{content.introSection}</p>
-      </div>
-      <Image src="" alt="Placeholder" fluid style={{ width: "50%" }} />
-    </div>,
-
-    // Professional Journey: PayPal Part 1
-    <div className="text-start vh-100 scroll-snap-start">
-      <h2>{content.professionalJourneySection.title}</h2>
-      <h5 className="font-weight-bold">{content.professionalJourneySection.paypalSubSection.title}</h5>
-      <h6 className="font-italic">{content.professionalJourneySection.paypalSubSection.date}</h6>
-      <p>{content.professionalJourneySection.paypalSubSection.paragraph1}</p>
-    </div>,
-
-    // Professional Journey: PayPal Part 2
-    <div className="text-start vh-100 scroll-snap-start">
-      <p>{content.professionalJourneySection.paypalSubSection.paragraph2}</p>
-      <p>{content.professionalJourneySection.paypalSubSection.paragraph3}</p>
-    </div>,
-
-    // Professional Journey: Knoldus
-    <div className="text-start vh-100 scroll-snap-start">
-      <h5 className="font-weight-bold">{content.professionalJourneySection.knoldusSubSection.title}</h5>
-      <h6 className="font-italic">{content.professionalJourneySection.knoldusSubSection.date}</h6>
-      <p>{content.professionalJourneySection.knoldusSubSection.paragraph1}</p>
-      <p>{content.professionalJourneySection.knoldusSubSection.paragraph2}</p>
-      <p>{content.professionalJourneySection.knoldusSubSection.paragraph3}</p>
-      <p>{content.professionalJourneySection.knoldusSubSection.paragraph4}</p>
-    </div>,
-
-    // Professional Journey: Mitacs
-    <div className="text-start vh-100 scroll-snap-start">
-      <h5 className="font-weight-bold">{content.professionalJourneySection.mitacsSubsSection.title}</h5>
-      <h6 className="font-italic">{content.professionalJourneySection.mitacsSubsSection.date}</h6>
-      <p>{content.professionalJourneySection.mitacsSubsSection.paragraph1}</p>
-      <p>{content.professionalJourneySection.mitacsSubsSection.paragraph2}</p>
-    </div>
-  ];
 
   return (
-    <div className="overflow-scroll" style={{ scrollSnapType: "y mandatory", height: "100vh" }}>
-      
-      <WelcomeBanner />
+    <div className="home-page-container">
 
-      {chunks.map((chunk, index) => (
-        <div key={index} className="scroll-snap-start">
-          {chunk}
+      <div className="chunk">
+        <WelcomeBanner />
+      </div>
+
+      <div className="chunk">
+        <div>
+          <p>{content.introSection}</p>
         </div>
-      ))}
+      </div>
+
+      <div className="chunk">
+        <h2>{content.professionalJourneySection.title}</h2>
+        <h5 className="font-weight-bold">{content.professionalJourneySection.paypalSubSection.title}</h5>
+        <h6 className="font-italic">{content.professionalJourneySection.paypalSubSection.date}</h6>
+        <p>{content.professionalJourneySection.paypalSubSection.paragraph1}</p>
+      </div>
+
+      <div className="chunk">
+        <p>{content.professionalJourneySection.paypalSubSection.paragraph2}</p>
+        <p>{content.professionalJourneySection.paypalSubSection.paragraph3}</p>
+      </div>
+
+      <div className="chunk">
+        <h5 className="font-weight-bold">{content.professionalJourneySection.knoldusSubSection.title}</h5>
+        <h6 className="font-italic">{content.professionalJourneySection.knoldusSubSection.date}</h6>
+        <p>{content.professionalJourneySection.knoldusSubSection.paragraph1}</p>
+        <p>{content.professionalJourneySection.knoldusSubSection.paragraph2}</p>
+        <p>{content.professionalJourneySection.knoldusSubSection.paragraph3}</p>
+      </div>
+
+
+      <div className="chunk">
+        <h5 className="font-weight-bold">{content.professionalJourneySection.mitacsSubsSection.title}</h5>
+        <h6 className="font-italic">{content.professionalJourneySection.mitacsSubsSection.date}</h6>
+        <p>{content.professionalJourneySection.mitacsSubsSection.paragraph1}</p>
+        <p>{content.professionalJourneySection.mitacsSubsSection.paragraph2}</p>
+        <p>{content.professionalJourneySection.mitacsSubsSection.paragraph3}</p>
+      </div>
+
+
+
     </div>
   );
 };
