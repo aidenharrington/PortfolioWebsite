e24d2a87ce449ccea37a0b9f95cd080d07b3e384
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2025-02-14 12:33:49 -0800

    added styling to homepage professional journey section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -21,32 +21,44 @@ const HomePage = () => {
       </div>
 
       <div className="chunk">
-        <h2>{content.professionalJourneySection.title}</h2>
-        <h5 className="font-weight-bold">{content.professionalJourneySection.paypalSubSection.title}</h5>
-        <h6 className="font-italic">{content.professionalJourneySection.paypalSubSection.date}</h6>
-        <p>{content.professionalJourneySection.paypalSubSection.paragraph1}</p>
+        <div className="home-page-text-box">
+          <h2 className="professional-journey-title">{content.professionalJourneySection.title}</h2>
+          <h5 className="company-name">{content.professionalJourneySection.paypalSubSection.title}</h5>
+          <h6 className="job-title">{content.professionalJourneySection.paypalSubSection.jobTitle}</h6>
+          <h6 className="job-dates">{content.professionalJourneySection.paypalSubSection.date}</h6>
+          <p>{content.professionalJourneySection.paypalSubSection.paragraph1}</p>
+        </div>
       </div>
 
       <div className="chunk">
+      <div className="home-page-text-box">
+        <h5 className="company-name">{content.professionalJourneySection.paypalSubSection.titleContinued}</h5>
         <p>{content.professionalJourneySection.paypalSubSection.paragraph2}</p>
         <p>{content.professionalJourneySection.paypalSubSection.paragraph3}</p>
       </div>
+      </div>
 
       <div className="chunk">
-        <h5 className="font-weight-bold">{content.professionalJourneySection.knoldusSubSection.title}</h5>
-        <h6 className="font-italic">{content.professionalJourneySection.knoldusSubSection.date}</h6>
+      <div className="home-page-text-box">
+        <h5 className="company-name">{content.professionalJourneySection.knoldusSubSection.title}</h5>
+        <h6 className="job-title">{content.professionalJourneySection.knoldusSubSection.jobTitle}</h6>
+        <h6 className="job-dates">{content.professionalJourneySection.knoldusSubSection.date}</h6>
         <p>{content.professionalJourneySection.knoldusSubSection.paragraph1}</p>
         <p>{content.professionalJourneySection.knoldusSubSection.paragraph2}</p>
         <p>{content.professionalJourneySection.knoldusSubSection.paragraph3}</p>
       </div>
+      </div>
 
 
       <div className="chunk">
-        <h5 className="font-weight-bold">{content.professionalJourneySection.mitacsSubsSection.title}</h5>
-        <h6 className="font-italic">{content.professionalJourneySection.mitacsSubsSection.date}</h6>
+      <div className="home-page-text-box">
+        <h5 className="company-name">{content.professionalJourneySection.mitacsSubsSection.title}</h5>
+        <h6 className="job-title">{content.professionalJourneySection.mitacsSubsSection.jobTitle}</h6>
+        <h6 className="job-dates">{content.professionalJourneySection.mitacsSubsSection.date}</h6>
         <p>{content.professionalJourneySection.mitacsSubsSection.paragraph1}</p>
         <p>{content.professionalJourneySection.mitacsSubsSection.paragraph2}</p>
         <p>{content.professionalJourneySection.mitacsSubsSection.paragraph3}</p>
+      </div>
       </div>
 
 
